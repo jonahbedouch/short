e4a89118cf88eebae67337064cb85b9d35fa4aaa
Jonah Bedouch <jonah@bedouch.net>
2024-06-20 14:42:36 -0700

    Update admin sdk to work with vercel

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -1,12 +1,12 @@
 import { app } from 'firebase-admin';
-import { initializeApp, getApps, applicationDefault } from 'firebase-admin/app';
+import { initializeApp, getApps, applicationDefault, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { GoogleAuthProvider } from 'firebase/auth/web-extension';
 
 export function initAdmin() {
   if (getApps().length <= 0) {
     initializeApp({
-      credential: applicationDefault()
+      credential: cert(JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS as string))
     })
   }
 }
