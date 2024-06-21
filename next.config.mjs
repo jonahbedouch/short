2719cb80f4902dd9f56508de14d5e7f4d85f898b
Jonah Bedouch <jonah@bedouch.net>
2024-06-20 23:53:23 -0700

    Add CORS headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://l.bedouch.net'
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET, POST'
+                    }
+                ]
+            }
+        ]
+    }
+};
 
 export default nextConfig;
